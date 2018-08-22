3be8352de9fa055a769da29023b0c3c73c6e5b2a
Slippery <100019692@mvla.net>
2018-08-22 16:16:09 -0700

    Update Screen.java

--- Screen.java
+++ Screen.java
@@ -11,11 +11,23 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 public class Screen extends JPanel implements ActionListener
 {
-	ArrayList<Account> Accounts; 
+	ArrayList<Account> Accounts;
+
 	private JTextField firstName; 
 	private JTextField pinInput; 
-	private JButton firstNameButton; 
-	private JButton pinButton; 
+	private JTextField withdrawInput;
+	private JTextField depositInput; 
+
+	private JButton mainButton; 
+	private JButton withdraw; 
+	private JButton deposit; 
+
+	private boolean jennifer = false; 
+	private boolean jose = false; 
+	private boolean john = false; 
+	private boolean jill = false;
+	private boolean jacky = false;
+	private boolean wrongInput = false; 
 
 	public Screen()
 	{
@@ -27,22 +39,40 @@ public class Screen extends JPanel implements ActionListener
 			Accounts.add(new Account("Jill",600.01,4341));
 			Accounts.add(new Account("Jacky",910.01,4391));
 
-			firstName = new JTextField(); 
-			firstName.setBounds(100,300,100,30); 
-			this.add(firstName); 
 			pinInput = new JTextField(); 
 			pinInput.setBounds(600,300,100,30); 
 			this.add(pinInput); 
+			
+			firstName = new JTextField(); 
+			firstName.setBounds(100,300,100,30); 
+			this.add(firstName); 
 
-			firstNameButton =  new JButton("First Name");
-			firstNameButton.setBounds(100,350,100,30); 
-			firstNameButton.addActionListener(this);
-			this.add(firstNameButton); 
+			withdrawInput = new JTextField(); 
+			withdrawInput.setBounds(650,500,100,30); 
+			this.add(withdrawInput);
+			withdrawInput.setVisible(false);	
 
-			pinButton =  new JButton("Pin");
-			pinButton.setBounds(600,350,100,30); 
-			pinButton.addActionListener(this);
-			this.add(pinButton); 
+			depositInput = new JTextField(); 
+			depositInput.setBounds(650,425,100,30); 
+			this.add(depositInput);
+			depositInput.setVisible(false);
+
+			mainButton =  new JButton("Enter");
+			mainButton.setBounds(100,350,100,30); 
+			mainButton.addActionListener(this);
+			this.add(mainButton); 
+
+			withdraw =  new JButton("Withdraw");
+			withdraw.setBounds(650,465,100,30); 
+			withdraw.addActionListener(this);
+			this.add(withdraw); 
+			withdraw.setVisible(false);
+
+			deposit =  new JButton("Deposit");
+			deposit.setBounds(650,535,100,30); 
+			deposit.addActionListener(this);
+			this.add(deposit); 
+			deposit.setVisible(false);
 
 		this.setFocusable(true); 
 	}
@@ -66,6 +96,107 @@ public class Screen extends JPanel implements ActionListener
        	g.fillRect(100,100,600,150);
        	g.setColor(gunMetal);
        	g.drawString("Welcome to The Bank!",135,200);
+		if(jennifer)
+		{
+
+			pinInput.setVisible(false);
+			firstName.setVisible(false);	
+			mainButton.setVisible(false);
+
+			withdraw.setVisible(true);
+			deposit.setVisible(true);
+			withdrawInput.setVisible(true);
+			depositInput.setVisible(true);
+
+			g.setColor(livid);
+			g.setFont(a); 
+			double balance1 = Accounts.get(0).getBalance();
+			String name = Accounts.get(0).getName(); 
+			g.drawString("Welcome "+ name, 210,500);
+			g.drawString("Balance: "+ balance1, 225,550);
+		}
+
+		else if(jose)
+		{
+			pinInput.setVisible(false);
+			firstName.setVisible(false);	
+			mainButton.setVisible(false);
+
+			withdraw.setVisible(true);
+			deposit.setVisible(true);
+			withdrawInput.setVisible(true);
+			depositInput.setVisible(true);
+
+			g.setColor(livid);
+			g.setFont(a); 
+			double balance1 = Accounts.get(1).getBalance();
+			String name = Accounts.get(1).getName(); 
+			g.drawString("Welcome "+ name, 210,500);
+			g.drawString("Balance: "+ balance1, 225,550);
+		}
+		else if(john)
+		{
+			pinInput.setVisible(false);
+			firstName.setVisible(false);	
+			mainButton.setVisible(false);
+
+			withdraw.setVisible(true);
+			deposit.setVisible(true);
+			withdrawInput.setVisible(true);
+			depositInput.setVisible(true);
+
+			g.setColor(livid);
+			g.setFont(a); 
+			double balance1 = Accounts.get(2).getBalance();
+			String name = Accounts.get(2).getName(); 
+			g.drawString("Welcome "+ name, 210,500);
+			g.drawString("Balance: "+ balance1, 225,550);
+		}
+		else if(jill)
+		{
+			pinInput.setVisible(false);
+			firstName.setVisible(false);	
+			mainButton.setVisible(false);
+
+			withdraw.setVisible(true);
+			deposit.setVisible(true);
+			withdrawInput.setVisible(true);
+			depositInput.setVisible(true);
+
+			g.setColor(livid);
+			g.setFont(a); 
+			double balance1 = Accounts.get(3).getBalance();
+			String name = Accounts.get(3).getName(); 
+			g.drawString("Welcome "+ name, 210,500);
+			g.drawString("Balance: "+ balance1, 225,550);
+		}
+		else if(jacky)
+		{
+			pinInput.setVisible(false);
+			firstName.setVisible(false);	
+			mainButton.setVisible(false);
+
+			withdraw.setVisible(true);
+			deposit.setVisible(true);
+			withdrawInput.setVisible(true);
+			depositInput.setVisible(true);
+
+			g.setColor(livid);
+			g.setFont(a); 
+			double balance1 = Accounts.get(4).getBalance();
+			String name = Accounts.get(4).getName(); 
+			g.drawString("Welcome "+ name, 210,500);
+			g.drawString("Balance: "+ balance1, 225,550);
+		}
+		else if (wrongInput)
+		{
+			g.setColor(livid);
+			g.setFont(a); 
+			g.drawString("Account does not exist!", 210,500);
+
+		}
+
+		repaint();
 
           
     }
@@ -73,20 +204,81 @@ public class Screen extends JPanel implements ActionListener
     public void actionPerformed(ActionEvent e) 
     {
 
-   		if(e.getSource() == pinButton)
+   		if(e.getSource() == mainButton)
 		{
+
+			
+			String firstNameText = firstName.getText(); 
 			String inputText = pinInput.getText();
 			int result = Integer.parseInt(inputText);	
-		
+			
 
-			if(Accounts.get(0).getPin() == result)
+			if(firstNameText.equals("Jennifer") && result == 1234)
 			{
-				System.out.println("You're in"); 
+				jennifer = true; 
+				john = false; 
+				jose = false; 
+				jill = false; 
+				jacky = false; 
+		
+				Accounts.get(0).setAccessTrue();
+				System.out.println("Jennifer");
+
 			}
+
+			else if(firstNameText.equals("Jose") && result == 4321)
+			{
+				jose = true; 
+				john = false;  
+				jill = false; 
+				jacky = false; 
+				jennifer = false;
+				Accounts.get(1).setAccessTrue(); 
+				System.out.println("Jose"); 
+
+			}
+
+			else if(firstNameText.equals("John") && result == 4311)
+			{
+				john = true; 
+				jose = false; 
+				jill = false; 
+				jacky = false; 
+				jennifer = false;
+				Accounts.get(2).setAccessTrue(); 
+				System.out.println("John"); 
+			}
+			else if(firstNameText.equals("Jill") && result == 4341 )
+			{
+				jill = true;
+				john = false; 
+				jose = false; 
+				jacky = false; 
+				jennifer = false; 
+				Accounts.get(3).setAccessTrue();
+				System.out.println("Jill");
+			}
+			else if (firstNameText.equals("Jacky") && result == 4391)
+			{
+				jacky = true; 
+				john = false; 
+				jose = false; 
+				jill = false; 
+				jennifer = false;
+				Accounts.get(4).setAccessTrue();
+				System.out.println("Jacky");
+			}
+
 			else 
 			{
-				System.out.println("You are out");
+				john = false; 
+				jose = false; 
+				jill = false; 
+				jacky = false; 
+				jennifer = false;
+				wrongInput = true; 
 			}
+			
 		}
 
     }
